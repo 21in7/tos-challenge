c4227e91b590250e3e651242eb436ed9935154b7
21in7 <skawksmsduzo@gmail.com>
2023-12-02 08:24:30 +0900

    changes app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter as Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ChallengeComponent from './component/pages/Today';
 import TomorrowChallengeComponent from './component/pages/Tomorrow';
 import Main from './component/pages/Main';
 
 function App() {
     return (
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <Router>
                 <Routes>
                     <Route path="/" element={<Main />} />
                     <Route path="/today" element={<ChallengeComponent />} />
                     <Route path="/tomorrow" element={<TomorrowChallengeComponent />} />
                 </Routes>
-        </BrowserRouter>
+            </Router>
     );
 }
 
