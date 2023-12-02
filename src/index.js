ad93410c992284682d30cea0e4514a054ea3322b
21in7 <skawksmsduzo@gmail.com>
2023-12-02 09:02:21 +0900

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Router } from 'react-router-dom';
+import { HashRouter, Router } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
 
