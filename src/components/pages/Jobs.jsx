12b5964446687a1b88ecedb66243fa03e3dec45b
21in7 <skawksmsduzo@gmail.com>
2023-12-04 08:15:02 +0900

    fix error

--- src/components/pages/Jobs.jsx
+++ src/components/pages/Jobs.jsx
@@ -14,33 +14,35 @@ function JobsComponent() {
     const imageName = updatedJobs[jobType][0].icon + '.png';
     updatedJobs[jobType][0].localImageUrl = `/tos-challenge/icons/${imageName}`;
   });
+
+  // Move setJobs call outside of the forEach loop
   setJobs(updatedJobs);
-  
+
   // Check if jobs is not null or undefined before calling Object.keys
   const finalTopJobs = jobs ? Object.keys(jobs).slice(0, 5) : [];
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-        <Header />
+      <Header />
       <h1>직업 목록</h1>
       <Box display="flex" justifyContent="center" flexWrap="wrap">
-      {finalTopJobs.map((jobType, index) => (
-        <Box key={jobType} p={1} m={1} bgcolor="background.paper" borderRadius="borderRadius" boxShadow={1}>
-          <Link to={`/tos-challenge/${jobType}`}>
-            <Box sx={{ width: 100, height: 100 }}>
-              <CardMedia
-                component="img"
-                image={jobs[jobType] && jobs[jobType].length > 0 ? jobs[jobType][0].localImageUrl || 'default-placeholder-image.png' : 'default-placeholder-image.png'}
-                alt={jobType}
-              />
-            </Box>
-            <Box textAlign='center'>
-              {jobs[jobType] && jobs[jobType].length > 0 ? jobs[jobType][0].name : ''}
-            </Box>
-          </Link>
-        </Box>
-      ))}
-        </Box>
+        {finalTopJobs.map((jobType, index) => (
+          <Box key={jobType} p={1} m={1} bgcolor="background.paper" borderRadius="borderRadius" boxShadow={1}>
+            <Link to={`/tos-challenge/${jobType}`}>
+              <Box sx={{ width: 100, height: 100 }}>
+                <CardMedia
+                  component="img"
+                  image={jobs[jobType] && jobs[jobType].length > 0 ? jobs[jobType][0].localImageUrl || 'default-placeholder-image.png' : 'default-placeholder-image.png'}
+                  alt={jobType}
+                />
+              </Box>
+              <Box textAlign='center'>
+                {jobs[jobType] && jobs[jobType].length > 0 ? jobs[jobType][0].name : ''}
+              </Box>
+            </Link>
+          </Box>
+        ))}
+      </Box>
     </Box>
   );
 }
